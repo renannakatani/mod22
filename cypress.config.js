bdc7ffea17b9fe52641370fe7ec6567068e64d4e
renannakatani <101219067+renannakatani@users.noreply.github.com>
2023-02-28 17:42:27 -0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      
+    },
+    baseUrl: 'http://lojaebac.ebaconline.art.br',
+    specPattern: "**/*.feature",
+    specPattern: "**/**.cy.js"
+  },  
+});
